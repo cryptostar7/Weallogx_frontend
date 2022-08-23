835b72662094bdf411cbb27e6b7f993b76d45d34
hariom-eps <hariom@epixelsoftware.com>
2022-08-23 22:03:51 +0530

    first commit

---
+++ views/components/modal/DeleteReportModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal fade common-modal" id="deleteReportModal" tabindex="-1" aria-labelledby="deleteReportModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><img
+              src="images/icons/cross-grey.svg" class="img-fluid" alt="Close Modal"></button>
+        </div>
+        <div class="modal-body text-center">
+          <h5 class="modal-title fs-24 semi-bold-fw" id="deleteReportModalLabel">Delete Report?</h5>
+          <p class="fs-14">This action cannot be undone. To re-add a report you will have <br> to go back to Report
+            creation.</p>
+          <div class="d-inline-flex flex-column gap-13 pt-4 mt-2 pb-2">
+            <button type="button" class="btn yes-delete-btn">Yes, Delete</button>
+            <button type="button" class="btn modal-cancel-btn" data-bs-dismiss="modal">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
