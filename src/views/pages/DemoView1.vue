5daaa2a9208ed099bf6eadf83070593080f77781
hariom-eps <hariom@epixelsoftware.com>
2022-11-26 13:53:28 +0530

    check API

--- src/views/pages/DemoView1.vue
+++ src/views/pages/DemoView1.vue
@@ -15,23 +15,9 @@ export default {
   },
   methods: {
     getdata() {
-
-      const api = axios.create({
-        Accept: '*/*',
-        headers: { 
-          'Content-Type': 'application/json',
-          'Accept' : 'application/json',
-          },
-          withCredentials: true,  
-          responseType: 'json',
-        // proxy: {
-        //   host: process.env.REACT_APP_API_BASE_URL,
-        // },
-      });
-
-      let url = 'https://imperialadvance.my.salesforce.com/services/oauth2/token';
-
-      let data =  {
+      let url =
+        "https://imperialadvance.my.salesforce.com/services/oauth2/token";
+      let data = {
         grant_type: "password",
         client_id:
           "3MVG9JZ_r.QzrS7iR.meNd4qyMB0sH1xKBoY6ABT1aL8B7sagMTlhJtMQgKrekdraQgyyMv4j7LvKCAEi_fIF",
@@ -39,13 +25,32 @@ export default {
           "A7EBCE352D44E72F68CCDE41629C8D2D5DE149AAF41AECDF6FB5183D1EF1688E",
         username: "anil@imperialadvance.com",
         password: "BWWBkm(dupu*N@8T",
+       
       };
 
-       api.put(url, data).then((response) => {
-
-      }).catch((error) => {
-        console.log(error);
+      const api = axios.create({
+        Accept: "*/*",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          'Accept': "application/json",
+          "Access-Control-Allow-Credentials": "true",
+          'Authrization': "No Auth",
+        },
+         proxy: {
+          host: 'http://127.0.0.1:5173',
+        },
+        withCredentials: true,
+        responseType: "json",
+        baseURL: "https://imperialadvance.my.salesforce.com",
       });
+
+      api
+        .post(url, data)
+        .then(response => {})
+        .catch(error => {
+          console.log(error);
+        });
     },
   },
 };
