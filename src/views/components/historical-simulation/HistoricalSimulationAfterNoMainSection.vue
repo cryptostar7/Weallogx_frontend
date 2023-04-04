027a0ad809f62a7c9dbea85eac5115227496bceb
hariom-eps <hariom@epixelsoftware.com>
2023-04-04 11:17:47 +0530

    fix router issue in complate scenario section

--- src/views/components/historical-simulation/HistoricalSimulationAfterNoMainSection.vue
+++ src/views/components/historical-simulation/HistoricalSimulationAfterNoMainSection.vue
@@ -5,9 +5,9 @@ import { RouterLink } from "vue-router";
   <section class="main-section mt-0 historical-mainSection">
     <div class="reviewProgressMainDiv py-5 HistoricalPositionStatic historicalMarginBottom">
       <ul class="mt-1 review-progress" id="reviewProgress">
-        <li class="done"><router-link :to="`/scenario-details/${this.$route.params.scenario}`" class="nav-link p-0">Scenario Details</router-link></li>
-        <li class="done"><router-link :to="`/illustration-data/${this.$route.params.scenario}`" class="nav-link p-0">Illustration Data</router-link></li>
-        <li class="done"><router-link :to="`/comparative-vehicles/${this.$route.params.scenario}`" class="nav-link p-0">Comparative Vehicles</router-link></li>
+        <li class="done"><router-link :to="`/scenario-details/${$route.params.scenario}`" class="nav-link p-0">Scenario Details</router-link></li>
+        <li class="done"><router-link :to="`/illustration-data/${$route.params.scenario}`" class="nav-link p-0">Illustration Data</router-link></li>
+        <li class="done"><router-link :to="`/comparative-vehicles/${$route.params.scenario}`" class="nav-link p-0">Comparative Vehicles</router-link></li>
         <li class="active"><router-link to="" class="nav-link p-0">Historical Simulations</router-link></li>
       </ul>
       <router-link to="/" class="btn mt-1 ms-1">
@@ -27,9 +27,9 @@ import { RouterLink } from "vue-router";
               <p class="historicalAllSetPara">Your Scenario is all set!</p>
               
               <div class="text-center mt-30">
-                <router-link :to="`/review-summary/${this.$route.params.scenario}`" class="nav-link d-inline-block btn form-next-btn fs-14 active">Review</router-link>
+                <router-link :to="`/review-summary/${$route.params.scenario}`" class="nav-link d-inline-block btn form-next-btn fs-14 active">Review</router-link>
                 <span class="d-block mb-2"></span>
-                <router-link :to="`/select-historical-simulations/${this.$route.params.scenario}`" class="nav-link btn form-back-btn fs-14 active"><img
+                <router-link :to="`/select-historical-simulations/${$route.params.scenario}`" class="nav-link btn form-back-btn fs-14 active"><img
                     src="@/assets/images/icons/chevron-left-grey.svg" class="img-fluid" alt="Chevron" width="6"> Back</router-link>
               </div>
             </div>
--- src/views/pages/ReportBuilderPage.vue
+++ src/views/pages/ReportBuilderPage.vue
@@ -44,7 +44,6 @@ export default {
     CreateReportModal,
     CreateNewClientReportModal,
   },
-
 };
 </script>
 
