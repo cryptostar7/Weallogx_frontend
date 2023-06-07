13d6b60463fcefd9eb5f574a8100ab9cd011b9a5
VedPrakashEpixel <ved@epixelsoftware.com>
2023-06-07 17:28:26 +0530

    corrected the function timing of updating table header width

--- src/views/components/report/comparative-components/ComparativeTabComparativeTable.vue
+++ src/views/components/report/comparative-components/ComparativeTabComparativeTable.vue
@@ -928,7 +928,7 @@ export default {
         for (var i = 0; i < tables.length; i++) {
           tables[i].refreshHeaderWidth();
         }
-      }, 3000);
+      }, 1000);
     },
   },
   computed: {
