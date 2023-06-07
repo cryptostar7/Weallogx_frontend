83fe7bcda42a9f25c22e113944b91c6774179cf9
VedPrakashEpixel <ved@epixelsoftware.com>
2023-06-07 17:59:43 +0530

    removed the percentage sign from Custom Amount inputs in Enchancement tab

--- src/views/components/historical-simulation/CreditAndBonus.vue
+++ src/views/components/historical-simulation/CreditAndBonus.vue
@@ -27,7 +27,7 @@
                             <div class="or-div">
                                 or
                             </div>
-                            <div class="customAmountInputDiv ms-3">
+                            <div class="customAmountInputDiv customAmountNoPercent ms-3">
                                 <label for="customAmount">Custom Amount</label>
                                 <input type="text" class="onlyPositiveNum" @keyup="(e) => customAmount = e.target.value" min="1" :max="illustrateYear" ref="customInputRef">
                             </div>
