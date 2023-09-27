5e3626b0f54a1c9e0219c4fa0c43b8e7834b6ff9
hariom-eps <hariom@epixelsoftware.com>
2023-09-27 14:43:52 +0530

    Index strategies tab toggle issue fixed after populating the data using API

--- src/views/components/historical-simulation/HistoricalSimulationFromScratchMainSectionNew.vue
+++ src/views/components/historical-simulation/HistoricalSimulationFromScratchMainSectionNew.vue
@@ -1019,11 +1019,13 @@ export default {
           this.populateGlobalParameters(data);
           this.populateIndex(1, data.index_strategy_1);
           if (data.index_strategy_2) {
-            // this.tabs.tab2 = true;1
+            // this.tabs.tab2 = true;
+            this.setChecked('index_stategy_tab2');
             this.populateIndex(2, data.index_strategy_2);
           }
           if (data.index_strategy_3) {
             // this.tabs.tab3 = true;
+            this.setChecked('index_stategy_tab3');
             this.populateIndex(3, data.index_strategy_3);
           }
 
