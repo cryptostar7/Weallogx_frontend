9fb83941467ebf16c5b1f7128679a53e5fb7b33f
hariom-eps <hariom@epixelsoftware.com>
2023-10-26 08:33:38 +0530

    Checking PDF js issue

--- src/views/components/illustration/IllustrationDataMainSection.vue
+++ src/views/components/illustration/IllustrationDataMainSection.vue
@@ -308,14 +308,17 @@ import {
 } from "../../../services/helper.js";
 
 // Refernce URL - "https://mozilla.github.io/pdf.js/build/pdf.js";
-import "/src/assets/js/pdfjs-3.11.174/build/pdf.js";
+// import "/src/assets/js/pdfjs-3.11.174/build/pdf.js";
+import "https://mozilla.github.io/pdf.js/build/pdf.js";
+
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 const pdfjsLib = window["pdfjs-dist/build/pdf"];
 // The workerSrc property shall be specified.
 
 // Refernce URL - "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
-pdfjsLib.GlobalWorkerOptions.workerSrc = "/src/assets/js/pdfjs-3.11.174/build/pdf.worker.js";
+// pdfjsLib.GlobalWorkerOptions.workerSrc = "/src/assets/js/pdfjs-3.11.174/build/pdf.worker.js";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
 
 const fileReader = new FileReader();
 
