025682383d7921c6367c943d9cae154e460533c6
hariom-eps <hariom@epixelsoftware.com>
2024-05-06 11:35:17 +0530

    Added the client side simulation unique validation on clone edit modal

--- src/views/historical-simulations/components/modals/CloneHistoricalSimulationModal.vue
+++ src/views/historical-simulations/components/modals/CloneHistoricalSimulationModal.vue
@@ -1,17 +1,51 @@
 <template lang="">
-  <div class="modal fade common-modal" id="cloneSimulationModal" index="-1" aria-labelledby="cloneSimulationModal" aria-hidden="true">
+  <div
+    class="modal fade common-modal"
+    id="cloneSimulationModal"
+    index="-1"
+    aria-labelledby="cloneSimulationModal"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header pb-0">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-            <img src="@/assets/images/icons/cross-grey.svg" class="img-fluid" alt="Close Modal">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
+            <img
+              src="@/assets/images/icons/cross-grey.svg"
+              class="img-fluid"
+              alt="Close Modal"
+            />
           </button>
         </div>
         <div class="modal-body pt-0 text-center">
           <div class="d-flex align-items-center justify-content-center w-100">
-            <div class="d-flex align-items-center section-heading-bg modalHeadingDiv">
-              <button class="modalReportBuilderBr" v-if="clientDetail">{{ $sortName(clientDetail.firstname, clientDetail.lastname, clientDetail.middlename) }}</button>
-              <h2 class="modalReportBuilderBrTxt" v-if="clientDetail">{{ $clientName(clientDetail.firstname, clientDetail.lastname, clientDetail.middlename) }} <span>{{simulationDetails}}</span></h2>
+            <div
+              class="d-flex align-items-center section-heading-bg modalHeadingDiv"
+            >
+              <button class="modalReportBuilderBr" v-if="clientDetail">
+                {{
+                  $sortName(
+                    clientDetail.firstname,
+                    clientDetail.lastname,
+                    clientDetail.middlename
+                  )
+                }}
+              </button>
+              <h2 class="modalReportBuilderBrTxt" v-if="clientDetail">
+                {{
+                  $clientName(
+                    clientDetail.firstname,
+                    clientDetail.lastname,
+                    clientDetail.middlename
+                  )
+                }}
+                <span>{{ simulationDetails }}</span>
+              </h2>
             </div>
           </div>
           <div class="modalParaBorderDiv">
@@ -22,13 +56,37 @@
             <form action="">
               <div class="form-group">
                 <label for="reportBulder">Name Simulation</label>
-                <input type="text" class="form-control custom-control" v-model="name">
+                <input
+                  type="text"
+                  :class="`form-control custom-control ${
+                    uniqueError ? 'border_red' : ''
+                  }`"
+                  v-model="name"
+                />
+
+                <p v-if="uniqueError" class="text-danger">
+                  Simulation name already in use.
+                </p>
               </div>
             </form>
           </div>
           <div class="d-inline-flex flex-column gap-13 pt-4 mt-2 pb-2">
-            <button type="button" class="btn yes-delete-btn" data-bs-dismiss="modal" @click="cloneSimulation()">Clone this Simulation</button>
-            <button type="button" class="btn modal-cancel-btn" data-bs-dismiss="modal">Cancel</button>
+            <button
+              type="button"
+              class="btn yes-delete-btn"
+              data-bs-dismiss="modal"
+              :disabled="uniqueError"
+              @click="cloneSimulation()"
+            >
+              Clone this Simulation
+            </button>
+            <button
+              type="button"
+              class="btn modal-cancel-btn"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
@@ -43,10 +101,10 @@ export default {
   props: ["id", "client"],
   emits: ["cloneSimulation"],
   data() {
-    return { name: "" };
+    return { name: "", simulationNames: [] };
   },
   methods: {
-    cloneSimulation: function() {
+    cloneSimulation: function () {
       var id = this.$props.id;
       this.$store.dispatch("loader", true);
       post(
@@ -54,10 +112,10 @@ export default {
         { simulation_name: this.name },
         authHeader()
       )
-        .then(response => {
+        .then((response) => {
           this.$emit("cloneSimulation", id);
         })
-        .catch(error => {
+        .catch((error) => {
           if (
             error.code === "ERR_BAD_RESPONSE" ||
             error.code === "ERR_NETWORK"
@@ -69,11 +127,26 @@ export default {
           this.$store.dispatch("loader", false);
         });
     },
+    updateSimulationNames() {
+      let data = this.$store.getters.getHistoricalClientUsingId(
+        this.clientDetail.id
+      );
+      let results = [];
+      if (data && data.simulations) {
+        data.simulations.forEach((item) => {
+          if (item.simulation_details) {
+            results.push(item.simulation_details.name);
+          }
+        });
+      }
+
+      this.simulationNames = results;
+    },
   },
   computed: {
     clientDetail() {
       let clients = this.$store.state.data.historical_clients || [];
-      return clients.filter(item => item.id === this.$props.client)[0];
+      return clients.filter((item) => item.id === this.$props.client)[0];
     },
     simulationDetails() {
       let sid = this.$props.id;
@@ -82,7 +155,7 @@ export default {
         let simulation = {};
         if (detail && detail.simulations) {
           simulation = detail.simulations.filter(
-            item => Number(item.id) === Number(sid)
+            (item) => Number(item.id) === Number(sid)
           )[0];
         }
         if (simulation && simulation.simulation_details) {
@@ -92,15 +165,22 @@ export default {
       }
       return "";
     },
+    uniqueError() {
+      return this.simulationNames.includes(this.name.trim());
+    },
   },
   watch: {
     simulationDetails(e) {
       if (e) {
         this.name = `Copy of ${e}`;
+        this.updateSimulationNames();
       }
     },
   },
 };
 </script>
-<style lang="">
+<style>
+.border_red {
+  border: 1px solid #dc3545 !important;
+}
 </style>
