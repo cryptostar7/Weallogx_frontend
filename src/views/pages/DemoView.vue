2783fe8af8c10936cde4f66c64804e4c55627cb8
hariom-eps <hariom@epixelsoftware.com>
2024-05-30 12:00:18 +0530

    Added the stripe source get page for testing

--- src/views/pages/DemoView.vue
+++ src/views/pages/DemoView.vue
@@ -1,109 +1,183 @@
 <template lang="">
-  <div></div>
+  <div>
+    <navbar-component />
+    <section class="authMaindiv-area">
+      <div class="container middleContainer">
+        <div class="payment-methodMain-div">
+          <div class="payment-left-card-main-div">
+            <div class="payment-left-card">
+              <div class="paymentCardTopChipArea">
+                <img src="@/assets/images/user/card-chip.svg" alt="">
+                <p>CARD</p>
+              </div>
+              <p class="payment-left-card-number showingCardNumber">
+                XXXX&nbsp;&nbsp;&nbsp;&nbsp;XXXX&nbsp;&nbsp;&nbsp;&nbsp;XXXX&nbsp;&nbsp;&nbsp;&nbsp;XXXX</p>
+
+              <div class="cardExpiryDateCvvMainDiv">
+                <div class="cardExpiryDateDiv">
+                  <p>Expiry Date</p>
+                  <p class="showingExpiryDate">MM/YYYY</p>
+                </div>
+                <div class="cardCvvDiv">
+                  <p>CVV</p>
+                  <input type="password" class="ShowingCvv" readonly placeholder="***">
+                </div>
+              </div>
+              <p class="cardPlaceHolderName">CARD PLACE HOLDER NAME</p>
+            </div>
+          </div>
+          <form class="payment-right-card-main-div" @submit="getSource">
+            <div>
+              <div class="payment-right-heading-div">
+                <p>Add Payment Information</p>
+                <img src="@/assets/images/user/mastercard-logo.svg" alt="master card">
+                <img src="@/assets/images/user/visa-logo.svg" alt="master card">
+                <img src="@/assets/images/user/american-express-logo.svg" alt="master card">
+              </div>
+              <div class="paymentMethodFormDiv">
+                <div>
+                  <label for="card_no">Card Number</label>
+                  <div ref="cardNumber" id='card_no' class="numberInputs card-number"></div>
+                </div>
+                <div>
+                  <label for="name">Name</label>
+                  <input type="text" id='name' class="card-place-holder-name" v-model="cardHolder"
+                    placeholder="Card Place Holder Name" maxlength="39" />
+                </div>
+                <div class="expiryCvvInputDiv">
+                  <div>
+                    <label for="expiry_date">Expiry Date</label>
+                    <div id='expiry_date' ref="cardExpiry" class="card-expiry-date"></div><br />
+                  </div>
+                  <div class="cvvInputDiv">
+                    <label for="cvv_no">CVV</label>
+                    <div ref="cardCvc" id='cvv_no' class="numberInputs card-cvv-number"></div>
+                  </div>
+                </div>
+                <h1>{{surceId}}</h1>
+                <button type="submit" class="paymentformBtns">Save & Continue</button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div class="all-payment-methodMain-div">
+          <div>
+            <p class="allCardHeading">Cards</p>
+            <div class="allCardMainDiv">
+              <div class="allCardChipDiv">
+                <img src="@/assets/images/user/chip-black.svg" alt="Chip">
+                <p><span>**** **** **** 5277</span>&nbsp;&nbsp;|&nbsp;&nbsp;<span>Exp.&nbsp;</span><span>09/2023</span>
+                </p>
+              </div>
+              <div class="allCardChipDiv">
+                <router-link to="/edit-payment-method" class="paymentCardEditButton">Edit</router-link>
+                <a href="javascript:void(0)" class="paymentCardDeleteButton modalCommonCls"
+                  modalAttr="deleteModal">Delete</a>
+              </div>
+            </div>
+            <div class="allCardMainDiv">
+              <div class="allCardChipDiv">
+                <img src="@/assets/images/user/chip-black.svg" alt="Chip">
+                <p><span>**** **** **** 9024</span>&nbsp;&nbsp;|&nbsp;&nbsp;<span>Exp.&nbsp;</span><span>12/2026</span>
+                </p>
+              </div>
+              <div class="allCardChipDiv">
+                <router-link to="/edit-payment-method" class="paymentCardEditButton">Edit</router-link>
+                <a href="javascript:void(0)" class="paymentCardDeleteButton modalCommonCls"
+                  modalAttr="deleteModal">Delete</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <fotter-component />
+  </div>
 </template>
 <script>
-import { get } from "../../network/requests";
-import { getUrl } from "../../network/url";
-
+import NavbarComponent from "./../components/common/UserNavbarComponent.vue";
+import FotterComponent from "./../components/common/UserFooterComponent.vue";
+let stripe = Stripe(
+    `pk_test_51M3zSZSJJRL1HZKGqUikA8saFoEGb9nskOEzUWqIGaNYau1EAnR063C61dUyroh1smFz30gZLm5R3horE7S6HoN300svIlgfZa`
+  ),
+  elements = stripe.elements(),
+  card,
+  cardNumber,
+  cardExpiry,
+  cardCvc,
+  postalCode;
 export default {
+  components: { NavbarComponent, FotterComponent },
+  data() {
+    return {
+      user: false,
+      cardHolder: "",
+    };
+  },
   mounted() {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
+    if (this.$store.state.forms.temp_user) {
+      this.user = this.$store.state.forms.temp_user;
+    } 
+
+    var style = {
+      base: {
+        width: "100%",
+        height: "35px",
+        border: "none",
+        "border-bottom": "1px solid #DFDFDF",
+        "font-weight": "500",
+        "font-size": "18px",
+        color: "#202020",
+        "padding-right": "2rem",
+      },
     };
 
-    fetch(getUrl("comparative_report") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
+    cardNumber = elements.create("cardNumber", {
+      showIcon: true,
+      placeholder: "Card Number",
+      style: style.base,
+    });
+    cardExpiry = elements.create("cardExpiry");
+    cardCvc = elements.create("cardCvc");
+    postalCode = elements.create("postalCode");
 
-    fetch(getUrl("comparative_report_longevity") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
+    cardNumber.mount(this.$refs.cardNumber);
+    cardExpiry.mount(this.$refs.cardExpiry);
+    cardCvc.mount(this.$refs.cardCvc);
+  },
+  methods: {
+    getSource: async function(e) {
+      e.preventDefault();
 
-    fetch(getUrl("comparative_report_ending_value") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-
-    fetch(getUrl("comparative_report_death_benefit") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-
-    fetch(getUrl("comparative_report_longevity_ror") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-
-    fetch(getUrl("comparative_report_ending_value_ror") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-
-    fetch(getUrl("comparative_report_death_benefit_ror") + 380, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-
-
-    // get(getUrl("comparative_report") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_longevity") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_ending_value") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_death_benefit") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_longevity_ror") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_ending_value_ror") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    // get(getUrl("comparative_report_death_benefit_ror") + 380)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+      this.$store.dispatch("loader", true);
+      await stripe
+        .createSource(cardNumber, {
+          type: "card",
+          currency: "USD",
+          owner: {
+            name: this.cardHolder,
+          },
+        })
+        .then(response => {
+          if (response.source) {
+            this.surceId = response.source.id;
+            this.$store.dispatch("loader", false);
+          } else {
+            if (response.error) {
+              this.$toast.error(response.error.message);
+            } else {
+              this.$toast.error("Something went wrong!");
+            }
+            this.$store.dispatch("loader", false);
+          }
+        });
+    },
   },
 };
 </script>
-<style lang=""></style>
+<style>
+.ElementsApp input{
+    font-size: 67px!important;
+}
+</style>
