5bf6c8f9f6c9638fc3144e3096619512edd2f108
Jason Cooper <skawaii@gmail.com>
2025-03-16 00:13:03 -0400

    chore: remove dead code (#13)

--- src/views/components/user-dashboard/HeaderComponent.vue
+++ src/views/components/user-dashboard/HeaderComponent.vue
@@ -8,21 +8,6 @@
         <p>Trial Days Left</p>
         <p>2</p>
       </div>
-      <!-- <div class="DashBoardRightPara2Div">
-        <div class="infoParaMainDiv">
-          Cases Created
-          <span class="infoClass">
-            <img src="@/assets/images/user/dashboard-info-icon.svg" class="infoClass" alt="Icon"/>
-            <div class="infoMainDiv">
-              <div class="innerInfoDiv">
-                You have 10 free cases available in your Free Trial. Please upgrade now to avail more features.
-                <div class="rectangleDesign"></div>
-              </div>
-            </div>
-          </span>
-        </div>
-        <p>6 out of 10</p>
-      </div> -->
     </div>
   </div>
 </template>
