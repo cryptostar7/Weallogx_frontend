d2433025c1e8b41eebd03cfc38752e77e36cdb51
cryptostar7 <133494886+cryptostar7@users.noreply.github.com>
2025-07-01 14:07:34 -0400

    debugging full nginx.conf during build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 envsubst '$ALB_URL' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
+echo "---- NGINX CONFIG AT DEPLOY ----"
+cat /etc/nginx/conf.d/default.conf
+
 # Start NGINX
 echo "Starting NGINX..."
 exec nginx -g 'daemon off;' 
