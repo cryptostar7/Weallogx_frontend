7d08a98a15e61b52eebdac19d6048d99f33b0619
David Frenkiel <david.frenkiel@yahoo.com>
2025-09-17 16:19:24 -1000

    Added bar height calculations for new amount tab in retirement buffer results

--- src/views/retirement-buffer/income-rider/components/result/AnnualIncomeRiderAmountTab.vue
+++ src/views/retirement-buffer/income-rider/components/result/AnnualIncomeRiderAmountTab.vue
@@ -1,115 +1,129 @@
 <template lang>
-  <div class="row mt-5">
+  <div class="row mt-5" id="target-analysis-amount-tab">
 
     <div class="col-md-6">
       <div class="target-analysis-bar-main-div">
         <div class="d-flex flex-column">
           <div class="d-flex gap-4 justify-content-center w-100">
+
             <div class="target-analysis-bar">
               <div
                 class="target-analysis-inner-bar income-rider-inner-bar inner-clr1"
-                :style="`height: ${growthRateBarHeight()}%`"
+                :style="`height: ${balanceBarHeight()}%`"
               >
                 <div class="target-analysis-bottom-bar-area bottom-clr1">
                   <p>
                     <span class="bigBarNumberJsCls2"
-                      >{{ $percentFormat(inputs.growth_rate) }}%</span
+                      >{{ $numFormatWithDollar(inputs.total_balance) }}</span
                     >
                   </p>
                 </div>
               </div>
             </div>
-            <div class="target-analysis-bar">
+
+            <div class="target-analysis-bar stacked">
+
               <div
-                class="target-analysis-inner-bar income-rider-inner-bar inner-clr2"
-                :style="`height: ${optimalGrowthRateBarHeight()}%`"
+                class="target-analysis-inner-bar income-rider-inner-bar inner-clr3"
+                :style="`height: ${optimalBalanceBarHeight()[1]}%`"
               >
                 <div class="target-analysis-bottom-bar-area bottom-clr2">
                   <p>
                     <span class="bigBarNumberJsCls2"
                       >{{
-                        $percentFormat(
-                          irResult.optimization.optimal_growth_rate || 0
-                        )
-                      }}%</span
+                        $numFormatWithDollar(irResult.optimization.optimal_beginning_balance)
+                      }}</span
                     >
                   </p>
-                </div>
+                </div>                
+              </div>
+
+              <div
+                class="target-analysis-inner-bar income-rider-inner-bar inner-clr2"
+                :style="`height: ${optimalBalanceBarHeight()[0]}%`"
+              >
+                <div class="target-analysis-bottom-bar-area bottom-clr2">
+                  <p>
+                    <span class="bigBarNumberJsCls2"
+                      >{{ $numFormatWithDollar(inputs.total_balance) }}</span>
+                  </p>
+                </div>                
               </div>
             </div>
+
           </div>
+
           <p class="bar-long-para">
-            The required constant rate of return for the
-            {{ inputs.comparative_vehicle_account_name }} to match the
-            {{ inputs.income_rider_account_name }}’s income production is
-            <span
-              >{{
-                $percentFormat(irResult.optimization.optimal_growth_rate || 0)
-              }}%</span
-            >.
+            The required beginning balance for the {{ inputs.comparative_vehicle_account_name }}
+            earning a flat {{ $percentFormat(inputs.growth_rate || 0) }}%
+            to match the {{ inputs.income_rider_account_name }}'s income production is
+            {{ $numFormatWithDollar(irResult.optimization.optimal_beginning_balance) }}.
           </p>
         </div>
       </div>
     </div>
 
-
-
-
-
-    <div class="col-md-6">
+    <div class="col-md-6 position-relative border-before-col">
       <div class="target-analysis-bar-main-div">
         <div class="d-flex flex-column">
           <div class="d-flex gap-4 justify-content-center w-100">
+
             <div class="target-analysis-bar">
               <div
                 class="target-analysis-inner-bar income-rider-inner-bar inner-clr1"
-                :style="`height: ${growthRateBarHeight()}%`"
+                :style="`height: ${balanceBarHeight()}%`"
               >
                 <div class="target-analysis-bottom-bar-area bottom-clr1">
                   <p>
                     <span class="bigBarNumberJsCls2"
-                      >{{ $percentFormat(inputs.growth_rate) }}%</span
+                      >{{ $numFormatWithDollar(inputs.total_balance) }}</span
                     >
                   </p>
                 </div>
               </div>
             </div>
-            <div class="target-analysis-bar">
+
+            <div class="target-analysis-bar stacked">
+
               <div
-                class="target-analysis-inner-bar income-rider-inner-bar inner-clr2"
-                :style="`height: ${optimalGrowthRateBarHeight()}%`"
+                class="target-analysis-inner-bar income-rider-inner-bar inner-clr3"
+                :style="`height: ${historicalOptimalBalanceBarHeight()[1]}%`"
               >
                 <div class="target-analysis-bottom-bar-area bottom-clr2">
                   <p>
                     <span class="bigBarNumberJsCls2"
                       >{{
-                        $percentFormat(
-                          irResult.optimization.optimal_growth_rate || 0
-                        )
-                      }}%</span
+                        $numFormatWithDollar(irHistoricalResult.optimization.optimal_beginning_balance)
+                      }}</span
                     >
                   </p>
-                </div>
+                </div>                
+              </div>
+
+              <div
+                class="target-analysis-inner-bar income-rider-inner-bar inner-clr2"
+                :style="`height: ${historicalOptimalBalanceBarHeight()[0]}%`"
+              >
+                <div class="target-analysis-bottom-bar-area bottom-clr2">
+                  <p>
+                    <span class="bigBarNumberJsCls2"
+                      >{{ $numFormatWithDollar(inputs.total_balance) }}</span>
+                  </p>
+                </div>                
               </div>
             </div>
+
           </div>
           <p class="bar-long-para">
-            The required constant rate of return for the
-            {{ inputs.comparative_vehicle_account_name }} to match the
-            {{ inputs.income_rider_account_name }}’s income production is
-            <span
-              >{{
-                $percentFormat(irResult.optimization.optimal_growth_rate || 0)
-              }}%</span
-            >.
+            The required beginning balance for the {{ inputs.comparative_vehicle_account_name }}
+            using historical returns for the last [number of years]
+            years to match the {{ inputs.income_rider_account_name }}'s
+            income production is
+            {{ $numFormatWithDollar(irHistoricalResult.optimization.optimal_beginning_balance) }}.
           </p>
         </div>
       </div>
     </div>
-
-
-
-
 
   </div>
 </template>
@@ -121,46 +135,56 @@ export default {
   props: ["currentTab"],
   data() {
     return {
-      success_required: 0,
-      pieData: {
-        speed: 30,
-        percent: 0,
-        colorSlice: "#4A8ECD",
-        colorCircle: "#9DC6EB",
-        round: true,
-      },
     };
   },
   mounted() {
-    // if (this.irSimulationResult) {
-    //   this.pieData.percent = this.irSimulationResult.success_percentage;
-    //   this.success_required = this.irSimulationResult.success_count;
-    // }
   },
   methods: {
-    growthRateBarHeight() {
-      let value1 = Number(this.irResult.optimization.optimal_growth_rate);
-      let value2 = Number(this.inputs.growth_rate);
 
-      if (value2 >= value1) {
-        return 100;
-      }
-
-      let unit = value2 / value1;
-      return unit * 100;
+    balanceBarHeight() {
+      const totalBalance = Number(this.inputs.total_balance);
+      const optimalTotalBalance = Number(this.irResult.optimization.optimal_beginning_balance);
+      return totalBalance / optimalTotalBalance * 100
     },
-    optimalGrowthRateBarHeight() {
-      let value1 = Number(this.inputs.growth_rate);
-      let value2 = Number(this.irResult.optimization.optimal_growth_rate);
 
-      if (value2 >= value1) {
-        return 100;
+    optimalBalanceBarHeight() {
+
+      const totalBalance = Number(this.inputs.total_balance);
+      const optimalTotalBalance = Number(this.irResult.optimization.optimal_beginning_balance);
+
+      if (optimalTotalBalance > totalBalance) {
+
+        const bottomHeight = totalBalance / optimalTotalBalance * 100
+        const topHeight = 100 - bottomHeight
+        return [bottomHeight, topHeight]
+
+      } else {
+        return [0, 0]
       }
+    },
 
-      let unit = value2 / value1;
-      return unit * 100;
+    historicalOptimalBalanceBarHeight() {
+
+      const totalBalance = Number(this.inputs.total_balance);
+      const optimalTotalBalance = Number(this.irResult.optimization.optimal_beginning_balance);
+
+      const historicalOptimalTotalBalance =
+            Number(this.irHistoricalResult.optimization.optimal_beginning_balance);
+
+      if (historicalOptimalTotalBalance > totalBalance) {
+
+        const bottomHeight = totalBalance / optimalTotalBalance * 100
+        const topHeight = (historicalOptimalTotalBalance - totalBalance) /
+                          optimalTotalBalance * 100
+
+        return [bottomHeight, topHeight]
+
+      } else {
+        return [0, 0]
+      }
     },
   },
+
   computed: {
     ...mapState({
       inputs: (state) => state.incomeRider.result.inputs || [],
@@ -168,7 +192,7 @@ export default {
     }),
     ...mapGetters({
       irResult: "incomeRider/irResult",
-    //   irSimulationResult: "incomeRider/irSimulationResult",
+      irHistoricalResult: "incomeRider/irHistoricalResult",
     }),
   },
   watch: {
